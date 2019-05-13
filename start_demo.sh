e7920e57b399f64e9e02a55dd889b7ae5732af83
Roberto Zegers <rzegers@puc.cl>
2019-05-13 05:42:15 -0700

    Add ar_track_alvar to detect AR Tags and localize them in space

--- start_demo.sh
+++ start_demo.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 echo ""
-echo "Sortation System Simulation"
+echo "ROS Sortation System Project Demo"
 echo ""
 xterm  -e  "roslaunch sortation_system gazebo.launch" &
-sleep 8s
+sleep 10
 
 xterm -e "roslaunch sortation_system spawn_conveyor.launch" &
-sleep 4s &
+sleep 4 &
 
 xterm -e "roslaunch sortation_system spawn_portal_frame.launch" &
-sleep 4s &
+sleep 4 &
 
 xterm -e "roslaunch sortation_system spawn_camera_link.launch" &
-sleep 4s &
+sleep 4 &
 
-xterm -e "roslaunch sortation_system rviz.launch"
+# Launches a box with AR tags on each side
+xterm -e "roslaunch sortation_system spawn_box.launch" &
+sleep 6 &
 
+# start RVIZ with a preconfigured view
+xterm -e "roslaunch sortation_system rviz.launch" &
+sleep 4 &
+
+# launch ar_track_alvar to detect AR markers and localize them in space
+xterm -e "roslaunch sortation_system ar_track.launch" &
+
+sleep 4 &
+xterm -e "rosservice call /gazebo/unpause_physics '{}'"
