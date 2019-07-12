c16c1ae91aa5a6e9e0e873f3dbc9070c27bbf4ed
Roberto Zegers <rzegers@puc.cl>
2019-07-12 03:46:37 -0700

    Add prompt to press any key to continue once Gazebo has loaded

--- start_demo.sh
+++ start_demo.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 echo ""
 echo "ROS Sortation System Project Demo"
 echo ""
@@ -6,7 +7,11 @@ echo ""
 
 # Fire up Gazebo world with conveyor belt model & plugin
 xterm -e "roslaunch sortation_system conveyor.launch" &
-sleep 10
+sleep 8
+
+# Prompt for a key press to continue after Gazebo has loaded
+read -n 1 -r -s -p "Press any key to continue once Gazebo has loaded or Ctrl+C to abort..."
+echo ""
 
 # Activate the conveyor belt:
 xterm -e "rosservice call /conveyor/control 'state:
@@ -43,3 +48,7 @@ sleep 4
 
 xterm -e "rosservice call /gazebo/unpause_physics '{}'"
 
+# Prompt for a key press to continue..
+echo "Press any key to return to the shell prompt or"
+read -n 1 -r -s -p "Ctrl+C to close all X-Term processes..."
+echo ""
